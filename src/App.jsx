bd90bfa2236ebf76f787e07ce84fc51ee52d7c66
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-12 19:53:52 +0900

    [구현/태스트] App - route로 url 주소에 맞게 컴포넌트 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
+import {
+  Link,
+  Route, Routes,
+} from 'react-router-dom';
 import AboutPage from './AboutPage';
 import HomePage from './HomePage';
 import NotFoundPage from './NotFoundPage';
 import RestaurantsPage from './RestaurantsPage';
+import RestaurantPage from './RestaurantPage';
+import LoginPage from './LoginPage';
 
 export default function App() {
-  // console.log(window.location.pathname)
-  const { location: { pathname } } = window;
-
-  const MyComponent = {
-    '/': HomePage,
-    '/about': AboutPage,
-    '/restaurants': RestaurantsPage,
-  }[pathname] || NotFoundPage;
-
   return (
-    <MyComponent />
+    <>
+      <header>
+        <h1><Link to="/">헤더영역</Link></h1>
+      </header>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/restaurants" element={<RestaurantsPage />} />
+        <Route path="/restaurants/:id" element={<RestaurantPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+
+    </>
+
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,18 +4,63 @@ import { render } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('App', () => {
-  const dispatch = jest.fn();
+describe('App', () => {
+  beforeEach(() => {
+    const dispatch = jest.fn();
 
-  useDispatch.mockImplementation(() => dispatch);
+    useDispatch.mockImplementation(() => dispatch);
 
-  useSelector.mockImplementation((selector) => selector({
-    regions: [],
-    categories: [],
-    restaurants: [],
-  }));
-  // App렌더만 확인
-  render((<App />));
+    useSelector.mockImplementation((selector) => selector({
+      regions: [{
+        id: 1, name: '서울',
+      }],
+      categories: [],
+      restaurants: [],
+    }));
+  });
+  context('with path /', () => {
+    it('renders HomePage', () => {
+      const { container } = render((
+        <MemoryRouter initialEntries={['/']}>
+          <App />
+        </MemoryRouter>
+      ));
+      expect(container).toHaveTextContent('Home');
+      expect(container).toHaveTextContent('About');
+      expect(container).toHaveTextContent('Restaurants');
+    });
+  });
+  context('with path /about', () => {
+    it('renders AboutPage', () => {
+      const { container } = render((
+        <MemoryRouter initialEntries={['/about']}>
+          <App />
+        </MemoryRouter>
+      ));
+      expect(container).toHaveTextContent('About');
+    });
+  });
+  context('with path /restaurants', () => {
+    it('renders RestaurantsPage', () => {
+      const { getByRole } = render((
+        <MemoryRouter initialEntries={['/restaurants']}>
+          <App />
+        </MemoryRouter>
+      ));
+      expect(getByRole('button', { name: '서울' }));
+    });
+  });
+  context('with wrong path', () => {
+    it('renders NotFoundPage', () => {
+      const { container } = render((
+        <MemoryRouter initialEntries={['/wrong']}>
+          <App />
+        </MemoryRouter>
+      ));
+      expect(container).toHaveTextContent('404');
+    });
+  });
 });
