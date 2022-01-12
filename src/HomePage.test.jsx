d7ee9a950159e2a213b150b56a03e152a0e248da
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-12 19:54:29 +0900

    [구현/테스트] Homepage - About, Restaurants, Log In 링크

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { MemoryRouter } from 'react-router-dom';
 import HomePage from './HomePage';
 
 test('HomePage', () => {
   // HomePage렌더만 확인
-  render((<HomePage />));
+  render((
+    <MemoryRouter>
+      <HomePage />
+    </MemoryRouter>
+  ));
 });
