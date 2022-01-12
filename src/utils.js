8c36529f0c9ea7cd5ab9ceefbc001b28bcf486d2
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-12 20:02:08 +0900

    [구현] isEmptyObject

--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,6 @@ export function get(key) {
 export function equal(key, value) {
   return (obj) => obj[key] === value;
 }
+export function isEmptyObject(obj) {
+  return Object.values(obj).length === 0;
+}
